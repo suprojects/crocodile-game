95977e77e774385fd6c7ac1c4fb7a0a6d835228e
Roj <n00b@localhost:5931>
2020-12-10 10:59:11 +0300

    c

--- handlers/message.py
+++ handlers/message.py
@@ -17,12 +17,29 @@ def message(update, context, lang):
         host = cr_host(context)
 
         if type(host) != str:
-            if usr.id != host[0]:
-                if eq(msg.text, cr_word(context)):
+            if eq(msg.text, cr_word(context)):
+                if usr.id != host[0]:
                     add_score(update.effective_chat.id, usr.id, usr.username)
                     set_in_game(False, context)
                     msg.reply_text(
                         _(lang, "guessed").format(
+                            f'<a href="tg://user?id={usr.id}">{usr.full_name}</a>',
+                            f'<b>{cr_word(context)}</b>'
+                        ),
+                        parse_mode="HTML",
+                        reply_markup=InlineKeyboardMarkup(
+                            [
+                                [
+                                    InlineKeyboardButton(
+                                        _(lang, "next_game"),
+                                        callback_data="next_game")
+                                ]
+                            ]
+                        )
+                    )
+                else:
+                    msg.reply_text(
+                        _(lang, "host").format(
                             f'<a href="tg://user?id={usr.id}">{usr.full_name}</a>',
                             f'<b>{cr_word(context)}</b>'
                         ),
